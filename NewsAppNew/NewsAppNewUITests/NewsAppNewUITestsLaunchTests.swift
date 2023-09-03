3f71d900d6f6da70de270f2647c68478944a91fe
Artem Yershov <ershov041091@icloud.com>
2023-09-03 13:19:19 +0300

    initial commit

---
+++ NewsAppNew/NewsAppNewUITests/NewsAppNewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NewsAppNewUITestsLaunchTests.swift
+//  NewsAppNewUITests
+//
+//  Created by Artem Yershov on 03.09.2023.
+//
+
+import XCTest
+
+final class NewsAppNewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
