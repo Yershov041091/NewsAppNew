d9fdf34ab51043833ba7bad1917a10706074fabc
Artem Yershov <ershov041091@icloud.com>
2023-09-04 19:38:35 +0300

    no message

---
+++ NewsAppNew/NewsAppNew/ApiManager.swift
@@ -0,0 +1,14 @@
+//
+//  ApiManager.swift
+//  NewsAppNew
+//
+//  Created by Artem Yershov on 04.09.2023.
+//
+
+import Foundation
+
+final class ApiManager {
+    func getNews(complition: @escaping () -> ()) {
+        
+    }
+}
